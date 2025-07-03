9d0dc9e533889a2c52c592fc8f09a8f5a8ea4ac0
SMH642800 <SMH642800@gmail.com>
2025-07-03 23:11:17 +0800

    Update app/models.py code
    Delete useless code.

--- app/db.py
+++ app/db.py
@@ -1,8 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "sqlite:///db.sqlite"
-DATABASE_URL = "sqlite:///test_db.sqlite"
+DATABASE_URL = "sqlite:///db.sqlite"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
